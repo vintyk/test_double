0879cf3fb9b6f1153e3f3884f328e3931e4e1036
Vinty <vinty1978@gmail.com>
2017-05-22 19:53:17 +0300

    Create Main

---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * Created by Vinty on 22.05.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HHHHHHHHHHHHHH");
+    }
+}
