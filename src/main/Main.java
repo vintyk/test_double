729448ad835ad1116e15975683a3b885a7ab66ab
Edenmond <dmitryrakovets@gmail.com>
2017-05-22 20:34:23 +0300

    add sout 2
    (cherry picked from commit 7d0996f)

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("HHHHHHHHHHHHHH");
         System.out.println("ниче не понимаю");
+        System.out.println(1);
     }
 }
