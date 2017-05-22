ba5576aaf198c70df0f21d50ac3e12cade09c237
Edenmond <dmitryrakovets@gmail.com>
2017-05-22 20:34:28 +0300

    Remove

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * Created by Vinty on 22.05.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HHHHHHHHHHHHHH");
+        System.out.println("ниче не понимаю");
+    }
+}
